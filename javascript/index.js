28b17db1befabc8fac576b92b9a2b1174b812747
Daaxes <daaxes@gmail.com>
2023-12-10 10:50:01 +0100

    test again

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,88 @@
+const weddingCakes = [
+  {
+    Id: "Cake1",
+    name: "8-tier Flower cake",
+    file: "./img/cakes/8-tierFlowerCake.jpg",
+    price: 342,
+    description: "Description",
+    generateHtml: function() {
+        return "<div class = 'productClass' Id = '" + this.Id + "' onclick='ShowTheProduct()'><img src='" + this.file + "' title = '" + this.name + "'><p>" + this.name + "</p><p>" + this.price + "&euro;</p></div>";
+  }}
+  //,
+  // {
+  //   Id: "Cake2",
+  //   name: "Gabriela cake",
+  //   file: "./img/cakes/Gabriela.jpg",
+  //   price: 290,
+  //   description: "Description",
+  //   generateHtml: function() {
+  //       return "<div class = 'productClass' Id = '" + this.Id + "' onclick='ShowTheProduct()'><img src='" + this.file + "' title = '" + this.name + "'><p>" + this.name + "</p><p>" + this.price + "&euro;</p></div>";
+  // }},  
+  // {
+  //   Id: "Cake3",
+  //   name: "Green leafs cake",
+  //   file: "./img/cakes/Green leafs.jpg",
+  //   price: 320,
+  //   description: "Description",
+  //   generateHtml: function() {
+  //       return "<div class = 'productClass' Id = '" + this.Id + "' onclick='ShowTheProduct()'><img src='" + this.file + "' title = '" + this.name + "'><p>" + this.name + "</p><p>" + this.price + "&euro;</p></div>";
+  // }},  
+  // {
+  //   Id: "Cake4",
+  //   name: "Belen cake",
+  //   file: "./img/cakes/Belen.jpg",
+  //   price: 420,
+  //   description: "Description",
+  //   generateHtml: function() {
+  //       return "<div class = 'productClass' Id = '" + this.Id + "' onclick='ShowTheProduct()'><img src='" + this.file + "' title = '" + this.name + "'><p>" + this.name + "</p><p>" + this.price + "&euro;</p></div>";
+  // }},  
+//   {
+//     Id: "Cake5",
+//     name: "Mr and Mrs cake",
+//     file: "./img/cakes/Mr and Mrs.jpg",
+//     price: 320,
+//     description: "Description",
+//     generateHtml: function() {
+//         return "<div class = 'productClass' Id = '" + this.Id + "' onclick='ShowTheProduct()'><img src='" + this.file + "' title = '" + this.name + "'><p>" + this.name + "</p><p>" + this.price + "&euro;</p></div>";
+//   }},
+//   {
+//     Id: "Cake6",
+//     name: "The Cat cake",
+//     file: "./img/cakes/The cat.jpg",
+//     price: 420,
+//     description: "Description",
+//     generateHtml: function() {
+//         return "<div class = 'productClass' Id = '" + this.Id + "' onclick='ShowTheProduct()'><img src='" + this.file + "' title = '" + this.name + "'><p>" + this.name + "</p><p>" + this.price + "&euro;</p></div>";
+//   }},
+//   {
+//     Id: "Cake7",
+//     name: "Tier rose cake",
+//     file: "./img/cakes/Tier rose.jpg",
+//     price: 642,
+//     description: "Description",
+//     generateHtml: function() {
+//         return "<div class = 'productClass' Id = '" + this.Id + "' onclick='ShowTheProduct()'><img src='" + this.file + "' title = '" + this.name + "'><p>" + this.name + "</p><p>" + this.price + "&euro;</p></div>";
+//   }},
+//   {
+//     Id: "Cake8",
+//     name: "White roses cake",
+//     file: "./img/cakes/White roses.jpg",
+//     price: 420,
+//     description: "Description",
+//     generateHtml: function() {
+//         return "<div class = 'productClass' Id = '" + this.Id + "' onclick='ShowTheProduct()'><img src='" + this.file + "' title = '" + this.name + "'><p>" + this.name + "</p><p>" + this.price + "&euro;</p></div>";
+//   }},
+//   {
+//   Id: "Cake9",
+//   name: "Tropical cake",
+//   file: "./img/cakes/Tropical.jpg",
+//   price: 640,
+//   description: "Description",
+//   generateHtml: function() {  
+//       return "<div class = 'productClass' Id = '" + this.Id + "' onclick='ShowTheProduct()'><img src='" + this.file + "' title = '" + this.name + "'><p>" + this.name + "</p><p>" + this.price + "&euro;</p></div>";
+// }}
+];
+
 const menuIcons = ["<img class='menu' id='menuInactiveID' src='./img/menu2.png' title='Menu'>", 
                    "<img class='menuMouseOver' id='menuActiveID' src='./img/menu.png' title='Menu' onmousedown='showMenu()'>"];
 
